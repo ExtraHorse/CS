a0ec500671dcf1e23f9d05d38925062114595461
Zachary Wang <zachwang101@gmail.com>
2018-10-17 21:02:32 -0400

    beans 2

--- GUI/src/advCSQ1/Pd5ZacharyWangMaze.java
+++ GUI/src/advCSQ1/Pd5ZacharyWangMaze.java
@@ -1,71 +1,100 @@
 package advCSQ1;
 
 import java.util.Scanner;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class Pd5ZacharyWangMaze extends JPanel {
+	Timer timer;
 	JButton[][] GUIGrid;
+	JButton start;
 	int StartX, StartY;
 	Scanner input = new Scanner(System.in);
+	ActionListener taskPerformer;
 	private int[][] grid = { { 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 }, { 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1 },
 			{ 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0 }, { 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1 },
 			{ 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1 }, { 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0 },
 			{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
 
-	//int [] [] grid = { {1,1,1}, {1,0,1},
+	// int [] [] grid = { {1,1,1}, {1,0,1},
 	// {0,0,1} };
-	
+
 	public Pd5ZacharyWangMaze() {
-		setLayout(new GridLayout(grid.length, grid[0].length));
+		setLayout(new BorderLayout());
+		JPanel center = new JPanel();
+		center.setLayout(new GridLayout(grid.length, grid[0].length));
 		GUIGrid = new JButton[grid.length][grid[0].length];
-		for (int r = 0; r < grid.length; r++)
+		for (int r = 0; r < grid.length; r++) {
 			for (int c = 0; c < grid[0].length; c++) {
 				JButton b = new JButton();
 				b.setEnabled(false);
-				if(grid[r][c] == 1)
-					b.setBackground(Color.black);
-				else
+				if (grid[r][c] == 1)
 					b.setBackground(Color.white);
+				else
+					b.setBackground(Color.black);
 				GUIGrid[r][c] = b;
-				this.add(b);
+				center.add(b);
 			}
+		}
+		add(BorderLayout.CENTER, center);
+		start = new JButton("START");
+		start.addActionListener(new Listener());
+		add(BorderLayout.SOUTH, start);
+		System.out.println("Enter start x and y");
 		StartX = input.nextInt();
 		StartY = input.nextInt();
-		add(this);
 	}
+
 	// Assumption: the exit is at the bottom right corner of the grid
 	//
 	public boolean findAnExit(int x, int y) {
 		String path = "START: ";
 		return findAnExitHelper(x, y, path);
 	} // FindAnExit
-	
-	public int[][] getMaze(){
+
+	private class Listener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			findAnExit(StartX, StartY);
+		}
+	}
+
+	public int[][] getMaze() {
 		return grid;
 	}
 
+	public void setButton(int x, int y) {
+		GUIGrid[y][x].setBackground(Color.blue);
+	}
+
 	public boolean findAnExitHelper(int x, int y, String path) {
-		System.out.println(path);
-		System.out.println(x + "," + y);
-		System.out.println(path.charAt(path.length() - 1));
+		setButton(x, y);
+		int ONE_SECOND = 1000;
+		timer = new Timer(ONE_SECOND, new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				setButton(x, y);
+			}
+		});
+		timer.start();
 		if (x == grid[0].length - 1 && y == grid.length - 1) {
 			System.out.println("true");
 			return true;
 		}
 		if (x > 0 && grid[y][x - 1] == 1 && path.charAt(path.length() - 1) != 'r')
-			if(findAnExitHelper(x - 1, y, path + "l"))
+			if (findAnExitHelper(x - 1, y, path + "l"))
 				return true;
 		if (x < grid[0].length - 1 && grid[y][x + 1] == 1 && path.charAt(path.length() - 1) != 'l')
-			if(findAnExitHelper(x + 1, y, path + "r"))
+			if (findAnExitHelper(x + 1, y, path + "r"))
 				return true;
 		if (y > 0 && grid[y - 1][x] == 1 && path.charAt(path.length() - 1) != 'd')
-			if(findAnExitHelper(x, y - 1, path + "u"))
+			if (findAnExitHelper(x, y - 1, path + "u"))
 				return true;
 		if (y < grid.length - 1 && grid[y + 1][x] == 1 && path.charAt(path.length() - 1) != 'u')
-			if(findAnExitHelper(x, y + 1, path + "d"))
+			if (findAnExitHelper(x, y + 1, path + "d"))
 				return true;
 		return false;
 	}
--- GUI/src/advCSQ1/Pd5ZacharyWangMazeMain.java
+++ GUI/src/advCSQ1/Pd5ZacharyWangMazeMain.java
@@ -6,6 +6,7 @@ public class Pd5ZacharyWangMazeMain {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Maze");
 		frame.setSize(500, 500);
+		frame.setLocation(200, 0);
 		frame.setContentPane(new Pd5ZacharyWangMaze());
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
